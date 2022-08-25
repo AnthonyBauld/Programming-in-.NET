41a19ab57ee03be245428650662c9e6132ebdc0b
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 11:56:10 -0400

    Create Rectangle.cs

---
+++ Assignment 2/Rectangle.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Author: Anthony. Bauld
+/// Date: October 6th, 2021
+/// Purpose: The purpose of the program is to output mathematical calulations based on the shape and values the user selects.
+/// Statement of Authorship: I, Anthony. Bauld, certify that this material is my original work. No other person's work has been used without due acknowledgement.
+/// </summary>
+
+namespace Lab2A
+{
+    class Rectangle : Shape
+    {
+        // This function creates a width variable.
+        private int Width;
+        // This function creates a length variable.
+        private int Length;
+
+        /// <summary>
+        /// Constructor for the Rectangle class, sets the default values.
+        /// </summary>
+        /// <param name="Type">A String for the shapes's name.</param>
+        /// <param name="Width">A int for the width of the shape.</param>
+        /// <param name="Length">A int for the shapes length.</param>
+        /// 
+
+        public Rectangle(String Type, int Width, int Length)
+        {
+            this.Type = Type;
+            this.Width = Width;
+            this.Length = Length;
+        }
+
+        /// <summary>
+        /// Writes lines to the console which allows use to input values for length and width.
+        /// </summary>
+        public override void SetData()
+        {
+            // Writes a line to the console asking for the length.
+            Console.WriteLine("\nEnter the length:");
+            // Converts length to a int and reads the console line.
+            Length = Convert.ToInt32(Console.ReadLine());
+            // Writes a line to the console asking for the width.
+            Console.WriteLine("Enter the width:");
+            // Converts width to a int and reads the console line.
+            Width = Convert.ToInt32(Console.ReadLine());
+        }
+        
+        /// <summary>
+        /// Calulates the area for rectangle.
+        /// </summary>
+        /// <returns>The area of a rectangle.</returns>
+        public override double CalculateArea()
+        {
+            return Length * Width;
+        }
+
+        /// <summary>
+        /// Create's a throw NotImplemtedExecption and places default values.
+        /// </summary>
+        /// <returns>This does not return anything.</returns>
+        public override double CalculateVolume()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a formatted ToString which will call the referrences SetData(), CalulateArea().
+        /// </summary>
+        /// <returns>The Rectangle outputs will be returned in a prepared string that may be used in Program.</returns>
+        public override string ToString()
+        {
+            return String.Format($"{Type} - L: {Length}, W: {Width}, Area: {CalculateArea()}");
+        }
+    }
+}
