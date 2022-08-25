03d282f44f35f12a98e04f1a78fd9fb4190e3c34
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 12:20:52 -0400

    Create ISearchable.cs

---
+++ Assignment 3/Part A/ISearchable.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// The class implementing the Search() method will be assumed to
+/// use a string search key as a parameter and return a boolean
+/// value to indicate if that key was successfully found.
+/// </summary>
+
+interface ISearchable
+{
+    bool Search(string key);
+}
+
